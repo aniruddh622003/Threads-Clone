aaf57a773e85036f00a0e5de1d82ce16aeb0e8f9
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2023-12-27 03:01:38 +0530

    Create User Endpoint Done

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,7 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'Username should not be empty' })
+  @IsString({ message: 'Username should be a string' })
+  username: string;
+}
